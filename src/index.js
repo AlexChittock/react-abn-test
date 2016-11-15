0225bb9943abf53d98d31e054b850ae6e8883452
Alex Chittock <alexchittock@gmail.com>
2016-11-15 09:20:39 +0000

    Exposed persistence

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 export Experiment from './experiment'
 export Variant from './variant'
+
+import LocalStoragePersistence from './persistence/local-storage'
+import NoPersistence from './persistence/none'
+import CookiePersistence from './persistence/cookie'
+import SessionPersistence from './persistence/session'
+
+export const Persistence = {
+  LocalStoragePersistence,
+  NoPersistence,
+  CookiePersistence,
+  SessionPersistence
+}
